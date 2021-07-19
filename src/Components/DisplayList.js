bc90369a8cfb0161d2f753c7330cbc9860080602
kellraym <ray.al.kell@gmail.com>
2021-07-19 16:31:13 -0600

    change fetch to http: again

--- src/Components/DisplayList.js
+++ src/Components/DisplayList.js
@@ -7,7 +7,7 @@ export default function DisplayList() {
 
 
   function getList() {
-    fetch(`http://http://crud-app-raykell.herokuapp.com/`)
+    fetch(`http://crud-app-raykell.herokuapp.com/`)
       .then(res => res.json())
       .then(json => setList(json))
   }
